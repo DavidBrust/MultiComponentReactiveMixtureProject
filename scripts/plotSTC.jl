5fe059b9bcb55befc8f7bc2f5276ccbadc11b6eb
Brust, David <david.brust@dlr.de>
2023-03-10 15:34:54 +0100

    plot STC

--- scripts/plotSTC.jl
+++ scripts/plotSTC.jl
@@ -4,11 +4,11 @@ using CSV, DataFrames, Plots
 df = CSV.read("./data/out/STC_bak.csv", DataFrame)
 
 p = Plots.plot(
-    xguide="Feed Flow / sccm",
+    xguide="Feed Flow / mL min-1",
     yguide="Solar Concentration / kW m-2",
     title="Solar-To-Chemical Efficiency",
     size=(400,266)
     )
 
 heatmap!(p,unique(df.Qflow)./ufac"cm^3/minute",unique(df.C),reshape(unique(df.STCs), 12,:),right_margin=5Plots.mm)
-#savefig(p,"./img/out/STC_bak.svg")
+savefig(p,"./img/out/STC_bak.svg")
