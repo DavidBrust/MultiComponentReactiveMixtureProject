d00549bf0238050e50d3ceb41bc09ba3404b6d74
Brust, David <david.brust@dlr.de>
2023-08-28 12:12:46 +0200

    2D Example.

--- notebooks/MSDiffDarcyOutflow.jl
+++ notebooks/MSDiffDarcyOutflow.jl
@@ -30,7 +30,7 @@ PlutoUI.TableOfContents(title="M-S Transport + Darcy")
 
 # ╔═╡ 57117131-8c83-4788-9f8e-f0b1b9404f0a
 function grid1D()
-	X=0:0.005:1
+	X=0:0.05:1
 	grid=simplexgrid(X)
 
 	# catalyst region
@@ -39,14 +39,35 @@ function grid1D()
 	grid
 end
 
+# ╔═╡ 173364be-0b52-445c-817d-f90b2a77e6e9
+function grid2D()
+	X=0:0.05:1
+	Y=0:0.2:1
+	grid=simplexgrid(X,Y)
+
+	# catalyst region
+	cellmask!(grid,[0.7,0.0],[0.9,1.0],2)
+	bfacemask!(grid, [1,0.2],[1,0.8],5)
+	
+	grid
+end
+
 # ╔═╡ 87630484-de43-46bc-a179-3e00b6e63c2a
-gridplot(grid1D()) 
+gridplot(grid2D())
 
 # ╔═╡ 0a911687-aff4-4c77-8def-084293329f35
 begin
 	# for 1D domain
-	const Γ_left = 1
-	const Γ_right = 2
+	#const Γ_left = 1
+	#const Γ_right = 2
+
+	# for 2D domain
+	const Γ_bottom = 1
+	#const Γ_right = 2
+	const Γ_right = 5
+	const Γ_top = 3
+	const Γ_left = 4
+	
 end;
 
 # ╔═╡ 2554b2fc-bf5c-4b8f-b5e9-8bc261fe597b
@@ -192,8 +213,7 @@ begin
 		#x[gni[:CO]] = 1.0
 		#x[gni[:CH4]] = 1.0
 		#x[gni[:H2O]] = 1.0
-		#x[gni[:N2]] = 1.0
-		#x[gni[:N2]] = 4.0
+		#x[gni[:N2]] = 1.0		
 		x/sum(x)
 	end # inlet composition	
 	
@@ -211,8 +231,8 @@ begin
 	# approximation from Wesselingh, J. A., & Krishna, R. (2006). Mass Transfer in Multicomponent Mixtures
 	γ_τ::Float64=ϕ^1.5 # constriction/tourtuosity factor
 
-	#k::Float64=1.23e-10*ufac"m^2" # permeability , por class 0
-	k::Float64=1.23e-12*ufac"m^2" # permeability , por class 0
+	k::Float64=1.23e-10*ufac"m^2" # permeability , por class 0
+	#k::Float64=1.23e-12*ufac"m^2" # permeability , por class 0
 	#k::Float64=1.23e-13*ufac"m^2" # permeability , por class 0
 
 
@@ -266,7 +286,6 @@ function reaction(f,u,node,data)
 		
 	ng=ngas(data)
 	# last species i=ng via ∑pi = p
-	#@views f[ng]=u[ip]-sum(u[1:ng])
 	for i=1:ng
 		f[ng] += u[i]
 	end
@@ -325,7 +344,7 @@ function bcond(f,u,bnode,data)
 	#inlet(f,u,bnode,data)
 
 	# specifying the inlet partial pressures converges
-	for i=1:ng
+	for i=1:ng	
 		boundary_dirichlet!(f,u,bnode, species=i,region=Γ_left,value=u[ip]*X0[i])
 	end
 	
@@ -351,59 +370,6 @@ function D_matrix!(data, D)
 	nothing
 end
 
-# ╔═╡ ed7941c4-0485-4d84-ad5b-383eb5cae70a
-function flux(f,u,edge,data)
-	(;ip,gni,X0,Tamb,k,Fluids,γ_τ) = data
-
-	ng=ngas(data)
-
-	F = MVector{ng-1,eltype(u)}(undef)
-
-	M = MMatrix{ng-1,ng-1,eltype(u)}(undef)
-	D = MMatrix{ng,ng,eltype(u)}(undef)
-
-	# compute total mass flux
-	for i=1:ng
-		f[ip] += Fluids[i].MW*0.5*(u[i,1]+u[i,2])/(ph"R"*Tamb)
-	end
-	f[ip] *=darcyvelo(u,data)
-		
-	@inline D_matrix!(data, D)
-
-	xi=zero(eltype(u))
-	xj=zero(eltype(u))
-	
-	# assemble matrix M and right hand side vector f
-	for i=1:(ng-1)
-		xi=(u[i,1]+u[i,2])/(u[ip,1]+u[ip,2])
-		# Matrix M
-		M[i,i] = 1/DK_eff(data,Tamb,i)		
-		M[i,i] += xi*Fluids[i].MW/Fluids[ng].MW/D[i,ng]
-	
-		for j=1:ng
-			if j != i
-				xj=(u[j,1]+u[j,2])/(u[ip,1]+u[ip,2])
-				M[i,i] += xj/D[i,j]
-
-				if j != ng
-					M[i,j] = -xi/D[i,j]
-					M[i,j] += xi*Fluids[j].MW/Fluids[ng].MW/D[i,ng]
-				end
-			end
-		end
-		# right hand side vector f
-		F[i] = (u[i,1]-u[i,2])/(ph"R"*Tamb) + xi*f[ip]/Fluids[ng].MW/D[i,ng]
-		
-	end
-			
-	# computation of fluxes N
-	@inline inplace_linsolve!(M,F)
-
-	for i=1:(ng-1)
-		f[i] = F[i]		
-	end	
-end
-
 # ╔═╡ 22447e9e-df5f-4773-a83e-8645ad0c7873
 function rho_mix_flux(u,data)
 	ng=ngas(data)
@@ -414,6 +380,58 @@ function rho_mix_flux(u,data)
 		rho += Fluids[i].MW*0.5*(u[i,1]+u[i,2])
 	end
 	rho /= ph"R"*Tamb	
+end
+
+# ╔═╡ ed7941c4-0485-4d84-ad5b-383eb5cae70a
+function flux(f,u,edge,data)
+	(;ip,gni,X0,Tamb,k,γ_τ) = data
+
+	ng=ngas(data)
+
+	F = MVector{ng-1,eltype(u)}(undef)
+
+	M = MMatrix{ng-1,ng-1,eltype(u)}(undef)
+	D = MMatrix{ng,ng,eltype(u)}(undef)
+
+	# compute total mass flux
+	@inline f[ip] =rho_mix_flux(u,data)*darcyvelo(u,data)
+		
+	@inline D_matrix!(data, D)
+
+	xi=zero(eltype(u))
+	xj=zero(eltype(u))
+
+	
+	# assemble matrix M and right hand side vector f
+	for i=1:(ng-1)
+		xi= (u[i,1]+u[i,2])/(u[ip,1]+u[ip,2])
+		# Matrix M
+		M[i,i] =  1.0/DK_eff(data,Tamb,i)		
+	
+		M[i,i] += xi*data.Fluids[i].MW/data.Fluids[ng].MW/D[i,ng]
+	
+		for j=1:ng
+			if j != i
+				xj=(u[j,1]+u[j,2])/(u[ip,1]+u[ip,2])
+				M[i,i] += xj/D[i,j]
+
+				if j != ng
+					M[i,j] = -xi/D[i,j]
+					M[i,j] += xi*data.Fluids[j].MW/data.Fluids[ng].MW/D[i,ng]
+				end
+			end
+		end
+		# right hand side vector f
+		F[i] = (u[i,1]-u[i,2])/(ph"R"*Tamb) + xi*f[ip]/data.Fluids[ng].MW/D[i,ng]
+		
+	end
+			
+	# computation of fluxes N
+	@inline inplace_linsolve!(M,F)
+
+	for i=1:(ng-1)
+		f[i] = F[i]		
+	end	
 end
 
 # ╔═╡ dd5e0a30-80ef-4eac-a014-b5a0f6a3c5fe
@@ -444,8 +462,10 @@ end
 # ╔═╡ 333b5c80-259d-47aa-a441-ee7894d6c407
 function main(;data=ModelData(),nref=0)
 
-	grid=grid1D()
-	(;ip,p,X0,Tamb)=data
+	#grid=grid1D()
+	grid=grid2D()
+	
+	(;ip,p,X0,)=data
 	ng=ngas(data)
 	
 	sys=VoronoiFVM.System( 	grid;
@@ -459,25 +479,24 @@ function main(;data=ModelData(),nref=0)
 							)
 	
 	enable_species!(sys; species=collect(1:(ng+1))) # gas phase species pi & ptotal	
-
-	lg=length(grid1D()[Coordinates])
 	
 	inival=unknowns(sys)
-	inival[:,:].=1.0*p
-	
+
+	inival[:,:].=1.0*p	
 	for i=1:ng
 		inival[i,:] .*= X0[i]
 	end
 	
 	# solve problem with parameter embedding (homotopy)
 	control = SolverControl(nothing, sys;)
-		control.Δp=1.0e-1
-		control.Δp_grow=2.0
+		control.Δp=1.0e-2
+		control.Δp_min=1.0e-6
+		control.Δp_grow=1.1
 		control.handle_exceptions=true
 		control.Δu_opt=1.0e5
 
 	embed=[0.0,1.0]
-	sol=solve(sys;inival=inival,control,embed,verbose="en")
+	sol=solve(sys;inival=inival,control,embed,verbose="aen")
 
 	sol,grid,sys,data
 end;
@@ -505,6 +524,7 @@ md"""
 # ╔═╡ 55f305b8-47a2-4fdf-b1cb-39f95f3dfa36
 #=╠═╡
 sol=sol_(t)
+#sol=sol_[1]
   ╠═╡ =#
 
 # ╔═╡ 8fcf636c-2330-4eda-9bb3-298e6a53dfd1
@@ -513,12 +533,12 @@ let
 	(;gni,ip,p)=data_embed
 	vis = GridVisualizer(legend = :rt, ylabel="Pressure / Pa")
 	scalarplot!(vis, grid, sol[ip,:], label="total")
-	scalarplot!(vis, grid, sol[gni[:N2],:], label="N2", color=:green, clear=false)
-	scalarplot!(vis, grid, sol[gni[:H2],:], label="H2", color=:red, clear=false)	
-	scalarplot!(vis, grid, sol[gni[:CO2],:], label="CO2", color=:blue, clear=false)
-	scalarplot!(vis, grid, sol[gni[:CH4],:], label="CH4", color=:purple, clear=false)
-	scalarplot!(vis, grid, sol[gni[:CO],:], label="CO", color=:orange, clear=false)
-	scalarplot!(vis, grid, sol[gni[:H2O],:], label="H2O", color=:cyan, clear=false)
+	#scalarplot!(vis, grid, sol[gni[:N2],:], label="N2", color=:green, clear=false)
+	#scalarplot!(vis, grid, sol[gni[:H2],:], label="H2", color=:red, clear=false)	
+	#scalarplot!(vis, grid, sol[gni[:CO2],:], label="CO2", color=:blue, clear=false)
+	#scalarplot!(vis, grid, sol[gni[:CH4],:], label="CH4", color=:purple, clear=false)
+	#scalarplot!(vis, grid, sol[gni[:CO],:], label="CO", color=:orange, clear=false)
+	#scalarplot!(vis, grid, sol[gni[:H2O],:], label="H2O", color=:cyan, clear=false)
 	reveal(vis)
 end
   ╠═╡ =#
@@ -528,10 +548,15 @@ md"""
 ## Flows Over Boundaries
 """
 
-# ╔═╡ 6aec25e2-9211-47d0-ba1b-542935660d91
-#=╠═╡
-data_embed.kinpar.gni
-  ╠═╡ =#
+# ╔═╡ 3c4b22b9-4f55-45b9-98d8-6cd929c737c2
+md"""
+1. CO
+1. H2
+1. CH4
+1. H2O
+1. CO2
+1. N2
+"""
 
 # ╔═╡ 2a4c8d15-168f-4908-b24b-8b65ec3ea494
 #=╠═╡
@@ -564,6 +589,7 @@ R=integrate(sys,reaction,sol)
 # ╠═11ac9b20-6a3c-11ed-0bb6-735d6fbff2d9
 # ╠═863c9da7-ef45-49ad-80d0-3594eca4a189
 # ╠═57117131-8c83-4788-9f8e-f0b1b9404f0a
+# ╠═173364be-0b52-445c-817d-f90b2a77e6e9
 # ╠═87630484-de43-46bc-a179-3e00b6e63c2a
 # ╠═0a911687-aff4-4c77-8def-084293329f35
 # ╟─2554b2fc-bf5c-4b8f-b5e9-8bc261fe597b
@@ -598,7 +624,7 @@ R=integrate(sys,reaction,sol)
 # ╠═8fcf636c-2330-4eda-9bb3-298e6a53dfd1
 # ╠═36d02d06-6d0d-4b98-8d60-f2df0afaeaad
 # ╟─8e0fdbc7-8e2b-4ae9-8a31-6f38baf36ef3
-# ╠═6aec25e2-9211-47d0-ba1b-542935660d91
+# ╟─3c4b22b9-4f55-45b9-98d8-6cd929c737c2
 # ╠═eb44075f-fbd3-4717-a440-41cb4eda8de1
 # ╠═2a4c8d15-168f-4908-b24b-8b65ec3ea494
 # ╠═e1602429-74fa-4949-bb0f-ecd681f52e42
